518020fca0f515f6fd01c10eff2dfe0f5a5df3a8
Atlantas <atlantasalisauskas2014@gmail.com>
2023-09-04 10:04:23 +0300

    s

---
+++ src/app/forecast.tsx
@@ -0,0 +1,7 @@
+const Forecast = () => {
+    return(
+        <div className="bg-white w-full h-[33%] border-solid rounded-md relative mt-[2%]"><h1>Ss</h1></div>
+    );
+}
+
+export default Forecast;
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Header from "./header";
+import Main from "./main";
+import Footer from "./footer";
+
+export default function Home() {
+  return (
+    <div className="w-full, h-screen">
+      <Header/>
+      <Main/>
+      <Footer/>
+    </div>
+  )
+}
