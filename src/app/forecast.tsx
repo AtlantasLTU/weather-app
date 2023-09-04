e500e98b392583a6986ef610feba68e9563eb380
Atlantas <atlantasalisauskas2014@gmail.com>
2023-09-04 10:01:32 +0300

    s

--- src/app/forecast.tsx
+++
@@ -1,7 +0,0 @@
-const Forecast = () => {
-    return(
-        <div className="bg-white w-full h-[33%] border-solid rounded-md relative mt-[2%]"><h1>S</h1></div>
-    );
-}
-
-export default Forecast;
