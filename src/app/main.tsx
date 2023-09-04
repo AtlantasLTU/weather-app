5ae1a55693acab2756780bc1c1ae6b6a25b7505f
Atlantas <atlantasalisauskas2014@gmail.com>
2023-09-04 09:06:04 +0300

    s

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,7 +2,7 @@ import Forecast from "./forecast";
 
 const Main = () =>{
     return(
-        <main className="bg-sky-400 w-full h-[86%] absolute top-[10%] justify-center">
+        <main className="bg-sky-400 w-full h-[86%] absolute top-[10%] flex justify-center">
             <div className="w-[800px] h-full absolute flex flex-col">
                 <Forecast></Forecast>
                 <Forecast></Forecast>
