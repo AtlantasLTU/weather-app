07cb2c2c2c5923c3cbc6d91c1b479d01ee8d6e91
Atlantas <atlantasalisauskas2014@gmail.com>
2023-09-04 09:54:55 +0300

    s

--- src/app/forecast.tsx
+++ src/app/forecast.tsx
@@ -1,6 +1,6 @@
 const Forecast = () => {
     return(
-        <div className="bg-white w-full h-[33%] border-solid rounded-md relative top-[2%] p-2"><h1>S</h1></div>
+        <div className="bg-white w-full h-[33%] border-solid rounded-md relative top-[2%] m-10"><h1>S</h1></div>
     );
 }
 
