717bca145cbcaa681276e1a0d16b5a7f921822ba
Atlantas <atlantasalisauskas2014@gmail.com>
2023-09-07 20:03:16 +0300

    07/09/2023 20:03 for Benukas(end of day basically)

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return(
         <footer className="relative w-full h-[4%] bg-primary">
             <div>
-                <h2>B</h2>
+                <h2></h2>
             </div>
         </footer>
     );
